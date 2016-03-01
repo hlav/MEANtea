10a04e710ad5a197502452c9eaa5cbaed9eaf333
Helene <helene@outlook.com>
2016-03-01 09:48:56 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+(function() {
+  var app = angular.module('gemStore', ['ngRoute', 'store-directives']);
+
+  app.controller('StoreController', ['$http', function($http){
+      var store = this;
+      store.products = [];
+      $http.get('/tea.json').success(function(data){
+        console.log(data);
+        store.products = data;
+      });
+    }]);
+
+  app.controller('ReviewController', function() {
+    this.review = {};
+
+    this.addReview = function(product) {
+      product.reviews.push(this.review);
+
+      this.review = {};
+    };
+  });
+})();
